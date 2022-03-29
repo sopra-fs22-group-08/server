5771accf5dccaf866d73201da4933522e900d109
arminveres <armin.veres@hotmail.com>
2022-03-29 16:58:41 +0200

    added Lombok Support
    - updated gradle to 7.4
    - adjusted formatting (4 tabs indent)
    - getters and setters can now be added through @Getter and @Setter
    annotation

--- src/main/java/ch/uzh/ifi/hase/soprafs22/rest/dto/UserGetDTO.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/rest/dto/UserGetDTO.java
@@ -1,43 +1,24 @@
 package ch.uzh.ifi.hase.soprafs22.rest.dto;
 
 import ch.uzh.ifi.hase.soprafs22.constant.UserStatus;
+import lombok.Getter;
+import lombok.Setter;
 
 public class UserGetDTO {
 
-  private Long id;
-  private String name;
-  private String username;
-  private UserStatus status;
+    @Getter
+    @Setter
+    private Long id;
 
-  public Long getId() {
-    return id;
-  }
+    @Setter
+    @Getter
+    private String name;
 
-  public void setId(Long id) {
-    this.id = id;
-  }
+    @Getter
+    @Setter
+    private String username;
 
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public UserStatus getStatus() {
-    return status;
-  }
-
-  public void setStatus(UserStatus status) {
-    this.status = status;
-  }
+    @Getter
+    @Setter
+    private UserStatus status;
 }
