f102c3851246df5122dc968c9269b029686c1876
RibelH <robert.hemenguel@hotmail.com>
2022-04-06 12:45:25 +0200

    WIP: First implementation of cards #54 closes #61

--- src/main/java/ch/uzh/ifi/hase/soprafs22/repository/CardRepository.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/repository/CardRepository.java
@@ -1,7 +1,5 @@
 package ch.uzh.ifi.hase.soprafs22.repository;
 
-import ch.uzh.ifi.hase.soprafs22.entity.Card;
-import ch.uzh.ifi.hase.soprafs22.entity.Deck;
 import ch.uzh.ifi.hase.soprafs22.entity.MultipleChoiceCard;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
