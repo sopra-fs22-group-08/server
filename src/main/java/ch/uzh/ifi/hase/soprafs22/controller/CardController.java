9b5d405a56f9e96297394599468fb0db2bf1f836
RibelH <robert.hemenguel@hotmail.com>
2022-04-06 19:58:53 +0200

    Added MCCardService closes #74

--- src/main/java/ch/uzh/ifi/hase/soprafs22/controller/CardController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/controller/CardController.java
@@ -6,10 +6,10 @@ import ch.uzh.ifi.hase.soprafs22.repository.DeckRepository;
 import ch.uzh.ifi.hase.soprafs22.repository.UserRepository;
 import ch.uzh.ifi.hase.soprafs22.rest.dto.MultipleChoiceCardPostDTO;
 import ch.uzh.ifi.hase.soprafs22.rest.mapper.DTOMapper;
+import ch.uzh.ifi.hase.soprafs22.service.MCCardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 public class CardController {
@@ -22,8 +22,14 @@ public class CardController {
     @Autowired
     private DeckRepository deckRepository;
 
+    private final MCCardService mccardService;
+
+    public CardController(MCCardService mccardService) {
+        this.mccardService = mccardService;
+    }
+
     /**
-     * @brief creates a single Deck based on given ID of user
+     * @brief creates a single Card based on given ID of user
      *        POST REQUEST: Status Code OK 200. IF fail Status Code -> 404 -> Not
      *        Found
      */
@@ -31,14 +37,11 @@ public class CardController {
     @ResponseStatus(HttpStatus.CREATED)
     public MultipleChoiceCard createCard(@PathVariable("deckId") Long deckId, @RequestBody MultipleChoiceCardPostDTO multipleChoiceCardPostDTO){
         //convert API deck to internal representation
-        MultipleChoiceCard cardRequest = DTOMapper.INSTANCE.convertMultipleChoiceCardPostDTOtoEntity(multipleChoiceCardPostDTO);
+        MultipleChoiceCard mcCardRequest = DTOMapper.INSTANCE.convertMultipleChoiceCardPostDTOtoEntity(multipleChoiceCardPostDTO);
         //create deck and save to deck repository
-        MultipleChoiceCard card = this.deckRepository.findById(deckId).map(deck -> {
-            cardRequest.setDeck(deck);
-            return cardRepository.save(cardRequest);
-        }).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Not found User with id = " + deckId));
+        MultipleChoiceCard mcCreatedCard = mccardService.createCard(deckId, mcCardRequest);
 
-        return card;
+        return mcCreatedCard;
     }
 
 
