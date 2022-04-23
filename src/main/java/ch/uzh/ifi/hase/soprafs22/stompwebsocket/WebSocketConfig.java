fd1632415b67e0c2e094d6545bb9ea3fe09050d9
arminveres <armin.veres@hotmail.com>
2022-04-23 15:48:52 +0200

    initial commit for websockets

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/stompwebsocket/WebSocketConfig.java
@@ -0,0 +1,39 @@
+package ch.uzh.ifi.hase.soprafs22.stompwebsocket;
+
+import ch.uzh.ifi.hase.soprafs22.stompwebsocket.interceptor.UserInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.setApplicationDestinationPrefixes("/app");
+        config.setUserDestinationPrefix("/users");
+        config.enableSimpleBroker("/topic", "/queue/invite");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/websocket")
+                .setAllowedOriginPatterns("*");
+
+        registry.addEndpoint("/websocket")
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new UserInterceptor());
+    }
+
+}
---
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/stompwebsocket/controller/InvitationController.java
@@ -0,0 +1,36 @@
+package ch.uzh.ifi.hase.soprafs22.stompwebsocket.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+
+/**
+ * GreetingController
+ */
+@Controller
+public class InvitationController {
+
+    @Autowired
+    private SimpMessagingTemplate simpMessagingTemplate;
+
+/*
+    @MessageMapping("/hello")
+    @SendTo("/topic/greetings")
+    public Greeting greeting(@Payload HelloMessage message) throws Exception {
+        Thread.sleep(1000);
+        return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+    }
+*/
+
+    @MessageMapping("/invite")
+    public void send(SimpMessageHeaderAccessor sha,
+                     @Payload String username) {
+        String greeting = "Hello from " + sha.getUser().getName();
+
+        simpMessagingTemplate.convertAndSendToUser(username, "/queue/invite/greetings", greeting);
+    }
+
+}
