df222b942f088a40d56dfc12501b8172c1aed6a0
arminveres <armin.veres@hotmail.com>
2022-05-14 12:22:17 +0200

    added fuzzy search mapping/feature

--- src/main/java/ch/uzh/ifi/hase/soprafs22/service/DeckService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/service/DeckService.java
@@ -62,15 +62,15 @@ public class DeckService {
      */
     public Deck updateDeck(Deck currentDeck, Deck deckInput) {
         String trimmedDeckname = deckInput.getDeckname().trim();
-        //check if deckname is empty or only whitespaces
-        if (deckInput.getDeckname() == null || trimmedDeckname.length()== 0) {
+        // check if deckname is empty or only whitespaces
+        if (deckInput.getDeckname() == null || trimmedDeckname.length() == 0) {
             String baseErrorMessage = "You cannot choose an empty Deckname!";
             throw new ResponseStatusException(HttpStatus.CONFLICT, String.format(baseErrorMessage));
         }
-        //set changes
+        // set changes
         currentDeck.setVisibility(deckInput.getVisibility());
         currentDeck.setDeckname(trimmedDeckname);
-        //save changes
+        // save changes
         deckRepository.save(currentDeck);
         deckRepository.flush();
         return currentDeck;
@@ -89,5 +89,9 @@ public class DeckService {
     public List<Deck> getDecksByVisibility(Visibility visibility) {
         List<Deck> publicDecksToBeReturned = this.deckRepository.findDeckByVisibility(visibility);
         return publicDecksToBeReturned;
+    }
+
+    public List<Deck> getDecksByFuzzyFind(String deckname) {
+        return deckRepository.findDeckByDecknameContainingIgnoreCase(deckname);
     }
 }
