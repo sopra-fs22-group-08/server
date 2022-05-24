f2e37b22cb07ae2eed60b9cc5f773397af23f5c1
arminveres <armin.veres@hotmail.com>
2022-05-24 17:07:49 +0200

    added DeckServiceIntegrationTest

--- src/test/java/ch/uzh/ifi/hase/soprafs22/service/DeckServiceIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/service/DeckServiceIntegrationTest.java
@@ -2,6 +2,9 @@ package ch.uzh.ifi.hase.soprafs22.service;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
 
 import ch.uzh.ifi.hase.soprafs22.constant.Visibility;
 import org.junit.jupiter.api.AfterEach;
@@ -11,6 +14,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.server.ResponseStatusException;
 
 import ch.uzh.ifi.hase.soprafs22.entity.Deck;
 import ch.uzh.ifi.hase.soprafs22.entity.User;
@@ -39,12 +43,12 @@ public class DeckServiceIntegrationTest {
     private UserService userService;
 
     private User testUser;
+    final String baseMsg = "does not match!";
 
     @BeforeEach
     public void setup() {
         // define testuser
         testUser = new User();
-        testUser.setId(1l);
         testUser.setFirstName("test");
         testUser.setLastName("name");
         testUser.setUsername("testUsername");
@@ -67,17 +71,88 @@ public class DeckServiceIntegrationTest {
         assertNull(deckRepository.findDeckByDeckname("testdeck"));
 
         Deck deckRequest = new Deck();
-        deckRequest.setId(1L);
         deckRequest.setUser(testUser);
         deckRequest.setDeckname("testdeck");
         deckRequest.setVisibility(Visibility.PUBLIC);
 
         Deck createdDeck = deckService.createDeck(testUser.getId(), deckRequest);
 
-        String baseMsg = "does not match!";
         assertEquals(deckRequest.getId(), createdDeck.getId(), "Deck ID" + baseMsg);
         assertEquals(deckRequest.getUser(), createdDeck.getUser(), "Saved User" + baseMsg);
         assertEquals(deckRequest.getDeckname(), createdDeck.getDeckname(), "Deckname" + baseMsg);
         assertEquals(deckRequest.getVisibility(), createdDeck.getVisibility(), "Visibility" + baseMsg);
+    }
+
+    @Test
+    public void updateDeck_success() {
+        Deck deckRequest = new Deck();
+        deckRequest.setUser(testUser);
+        deckRequest.setDeckname("testdeck");
+        deckRequest.setVisibility(Visibility.PUBLIC);
+        deckService.createDeck(testUser.getId(), deckRequest);
+
+        Deck inputDeck = new Deck();
+        inputDeck.setDeckname("newtestdeck");
+        inputDeck.setVisibility(Visibility.PRIVATE);
+
+        Deck updatedDeck = deckService.updateDeck(deckRequest, inputDeck);
+
+        assertEquals(deckRequest.getUser(), updatedDeck.getUser(), "Saved User " + baseMsg);
+        assertEquals(deckRequest.getDeckname(), updatedDeck.getDeckname(), "Deckname " + baseMsg);
+        assertEquals(deckRequest.getVisibility(), updatedDeck.getVisibility(), "Visibility " + baseMsg);
+    }
+
+    @Test
+    public void getDeckById_success() {
+        Deck deckRequest = new Deck();
+        deckRequest.setUser(testUser);
+        deckRequest.setDeckname("testdeck");
+        deckRequest.setVisibility(Visibility.PUBLIC);
+
+        deckService.createDeck(testUser.getId(), deckRequest);
+
+        Deck returnedDeck = deckService.getDeckById(deckRequest.getId());
+
+        assertEquals(deckRequest.getId(), returnedDeck.getId(), "Deck ID" + baseMsg);
+    }
+
+    @Test
+    public void getDecksByUserId_success() {
+        Deck deckRequest = new Deck();
+        deckRequest.setUser(testUser);
+        deckRequest.setDeckname("testdeck");
+        deckRequest.setVisibility(Visibility.PUBLIC);
+
+        deckService.createDeck(testUser.getId(), deckRequest);
+
+        List<Deck> returnedDeck = deckService.getDecksByUserId(testUser.getId());
+
+        assertEquals(deckRequest.getId(), returnedDeck.get(0).getId(), "Deck ID" + baseMsg);
+    }
+
+    @Test
+    public void deleteDeckById_success() {
+        Deck deckRequest = new Deck();
+        deckRequest.setUser(testUser);
+        deckRequest.setDeckname("testdeck");
+        deckRequest.setVisibility(Visibility.PUBLIC);
+        Deck createdDeck = deckService.createDeck(testUser.getId(), deckRequest);
+
+        deckService.deleteDeckById(createdDeck.getId());
+
+        assertThrows(ResponseStatusException.class, () -> deckService.getDeckById(createdDeck.getId()));
+    }
+
+    @Test
+    public void getDecksByFuzzyFind_success() {
+        Deck deckRequest = new Deck();
+        deckRequest.setUser(testUser);
+        deckRequest.setDeckname("testdeck");
+        deckRequest.setVisibility(Visibility.PUBLIC);
+        Deck createdDeck = deckService.createDeck(testUser.getId(), deckRequest);
+
+        List<Deck> foundDeck = deckService.getDecksByFuzzyFind("est");
+
+        assertEquals(createdDeck.getDeckname(), foundDeck.get(0).getDeckname(), "Deckname " + baseMsg);
     }
 }
